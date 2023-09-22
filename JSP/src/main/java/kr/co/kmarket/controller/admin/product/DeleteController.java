1cc6cda1c061e1486573baf56df8afb36fddaf9b
tngus78901 <tngus78901@gmail.com>
2023-09-22 17:12:23 +0900

    카테사진등록완료/ 메인사진3장추가예정/admin product list 수정삭제ing

--- JSP/src/main/java/kr/co/kmarket/controller/admin/product/DeleteController.java
+++ JSP/src/main/java/kr/co/kmarket/controller/admin/product/DeleteController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@WebServlet("/admin/delete.do")
+@WebServlet("/admin/product/register/delete.do")
 public class DeleteController extends HttpServlet {
 	private static final long serialVersionUID = -2546805682249228568L;
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -19,9 +19,28 @@ public class DeleteController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		
-		
+				String prodNo = req.getParameter("prodNo");
+				logger.debug("prodNo : " + prodNo);
+						
+				// 파일 삭제(DB)
+				int result = fService.deleteFile(no);
+				
+				// 글 삭제
+				aService.deleteArticle(no);
+						
+				// 파일 삭제(Directory)
+				if(result > 0) {
+					
+					String path	= aService.getFilePath(req);
+					
+					File file = new File(path+"/"+"파일명");
+					
+					if(file.exists()) {
+							file.delete();
+					}
+				}
+				// 리다이렉트
+				resp.sendRedirect("/admin/product/list.do");
+			}
 	}
-	
 
-}
