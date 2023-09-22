d46eb0825e072f0e07b79a599e2422094a3b3c1a
tngus78901 <tngus78901@gmail.com>
2023-09-22 10:59:35 +0900

    admin 상품목록 수정삭제 작업중

---
+++ JSP/src/main/java/kr/co/kmarket/controller/admin/product/DeleteController.java
@@ -0,0 +1,27 @@
+package kr.co.kmarket.controller.admin.product;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet("/admin/delete.do")
+public class DeleteController extends HttpServlet {
+	private static final long serialVersionUID = -2546805682249228568L;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+	}
+	
+
+}
