9314aabda2450d3c6939e161e75429a3ed19ccbb
hansan9611 <hansan9611@gmail.com>
2023-09-24 23:11:41 +0900

    자동로그인 진행중

--- JSP/src/main/java/kr/co/kmarket/controller/IndexController.java
+++ JSP/src/main/java/kr/co/kmarket/controller/IndexController.java
@@ -3,12 +3,14 @@ package kr.co.kmarket.controller;
 import kr.co.kmarket.dto.KmMemberDTO;
 import kr.co.kmarket.dto.KmProductCate1DTO;
 import kr.co.kmarket.dto.KmProductCate2DTO;
+import kr.co.kmarket.service.KmMemberService;
 import kr.co.kmarket.service.KmProductCate1Service;
 import kr.co.kmarket.service.KmProductCate2Service;
 import org.slf4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,10 +21,11 @@ import java.util.*;
 @WebServlet(value = {"", "/index.do"})
 public class IndexController extends HttpServlet {
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(IndexController.class);
+    //private KmMemberService service = KmMemberService.instance;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+    	
         req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 
